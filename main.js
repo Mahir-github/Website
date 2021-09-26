7fd69a72235fb0a7a4e00b80723238f3797a1b0d
Mahir-github <74800713+Mahir-github@users.noreply.github.com>
2021-09-25 20:21:05 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function MinecraftINFO() {
+    window.location = "News.html"
+} 
+function ApexSeer() {
+    window.open("https://www.ea.com/en-au/games/apex-legends");
+}
+function NaurtoFN() {
+    window.location = "New_Releases.html";
+}
